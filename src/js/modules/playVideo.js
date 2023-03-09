bf1d95fdcdbb45437165faa3f781713479dfbea9
IrynaKrt <rinakashi13@mail.ru>
2023-03-09 20:11:31 +0300

    fix: resolve youtube conflickt

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -89,7 +89,7 @@ export default class VideoPlayer {
         if (this.btns.length > 0) {
             const tag = document.createElement('script');
 
-            tag.src = "http://www.youtube.com/iframe_api";
+            tag.src = "https://www.youtube.com/iframe_api";
             const firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
